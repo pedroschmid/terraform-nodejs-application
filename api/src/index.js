9bb50360c213c0f70d6111f8581adbfdfed11b91
Pedro Abreu <pedro.abreu@vilaapps.com>
2021-07-29 21:22:58 -0300

    Added index.js and /health_check route of API

---
+++ api/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const server = express();
+
+server.use(cors());
+server.use(morgan('combined'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.get('/health_check', (_, res) => res.json({ message: 'Application its healthy =)'} ))
+
+server.listen(process.env.PORT || 3000, () => console.log('Server running'))
