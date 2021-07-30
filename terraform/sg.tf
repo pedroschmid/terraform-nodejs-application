dccbe460325f01caf98c5a55779e0364d744fc35
Pedro Abreu <pedro.abreu@vilaapps.com>
2021-07-29 22:42:45 -0300

    Added ecs security group

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -23,3 +23,30 @@ resource "aws_security_group" "alb" {
     "Name" = "nodejs-alb-sg"
   }
 }
+
+resource "aws_security_group" "ecs" {
+  name        = "nodejs-ecs-sg"
+  description = "Allows inbound traffic from ALB to ECS"
+
+  vpc_id = aws_vpc.nodejs.id
+
+  ingress {
+    description     = "ALB ingress"
+    protocol        = "tcp"
+    from_port       = 3000
+    to_port         = 3000
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = "nodejs-ecs-sg"
+  }
+}
