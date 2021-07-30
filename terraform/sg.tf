df3e43cd5c106453631a3c647bfd2a35277b1663
Pedro Abreu <pedro.abreu@vilaapps.com>
2021-07-29 22:13:28 -0300

    Added ALB security group allowing HTTP and HTTPS ingress traffic

---
+++ terraform/sg.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "alb" {
+  name        = "nodejs-alb-sg"
+  description = "Allows inboud traffic through ALB"
+  vpc_id      = aws_vpc.nodejs.id
+
+  ingress {
+    description = "HTTP ingress"
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS ingress"
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = "nodejs-alb-sg"
+  }
+}
