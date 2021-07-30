695746eb215e9b936c46d682a339352ba879645e
Pedro Abreu <pedro.abreu@vilaapps.com>
2021-07-29 22:56:18 -0300

    Added ECS task definition

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -2,3 +2,32 @@ resource "aws_ecs_cluster" "nodejs" {
   name = "nodejs-cluster"
 }
 
+resource "aws_ecs_task_definition" "nodejs" {
+  family                   = "nodejs"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+
+  container_definitions = jsonencode([
+    {
+      name      = "nodejs-container"
+      image     = aws_ecr_repository.nodejs.repository_url
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ],
+      "environment" = [
+        {
+          "name"  = "PORT",
+          "value" = "3000"
+        }
+      ]
+    }
+  ])
+}
