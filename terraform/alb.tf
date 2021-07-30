4f635be6d6ec018948443782c9be32aee3732480
Pedro Abreu <pedro.abreu@vilaapps.com>
2021-07-29 22:26:58 -0300

    Added ALB listener to HTTP

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -16,3 +16,14 @@ resource "aws_alb_target_group" "nodejs" {
   
   vpc_id = aws_vpc.nodejs.id
 }
+
+resource "aws_alb_listener" "http" {
+  load_balancer_arn = aws_alb.nodejs.arn
+  protocol = "HTTP"
+  port = 80
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_alb_target_group.nodejs.arn
+  }
+}
