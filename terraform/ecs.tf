5353af2be214d72c2a666c71902aa384b52bd3a4
Pedro Abreu <pedro.abreu@vilaapps.com>
2021-07-29 23:06:30 -0300

    Added task role and execution role arn to ECS task definition

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -8,7 +8,9 @@ resource "aws_ecs_task_definition" "nodejs" {
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
-
+  task_role_arn = aws_iam_role.ecs_host_role.arn
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
+  
   container_definitions = jsonencode([
     {
       name      = "nodejs-container"
