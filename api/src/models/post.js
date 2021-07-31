82fdd6c4a21fe93bca26591187004b756caa2d55
Pedro Abreu <pedro.abreu@vilaapps.com>
2021-07-30 22:30:57 -0300

    Added Post model

---
+++ api/src/models/post.js
@@ -0,0 +1,15 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const { sequelize } = require("../sequelize.js");
+
+const Post = sequelize.define("Post", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+module.exports = { Post };
