757640cbf685da5484473922195cb2d298ee2e0d
Pedro Abreu <pedro.abreu@vilaapps.com>
2021-07-30 22:41:28 -0300

    Added create method

--- api/src/controllers/post.js
+++ api/src/controllers/post.js
@@ -9,3 +9,14 @@ async function list(request, response, next) {
     return httpResponse(response, 500, error.message, {});
   }
 }
+
+async function create(request, response, next) {
+  const { title, text } = request.body;
+
+  try {
+    await Post.create({ title, text });
+    return httpResponse(response, 201, "Post created", { title, text });
+  } catch (error) {
+    return httpResponse(response, 500, error.message, {});
+  }
+}
